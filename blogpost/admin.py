074599e993f10ed5d343860395c19213f88160c3
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-23 16:15:58 +0300

    ft:created an admin class

--- blogpost/admin.py
+++ blogpost/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 
 from blogpost.models import Post,BlogComment
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+
 admin.site.register(BlogComment)
 admin.site.register(Post)
