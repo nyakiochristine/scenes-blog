eb489e40ef1f83778437dd7332e72b2749c5d578
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-23 14:48:05 +0300

    ft:created view function for the signup

--- home/views.py
+++ home/views.py
@@ -1,10 +1,14 @@
+import email
+import re
 from home.models import Contact
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'home.html')
     
 
 def contact(request):
@@ -22,4 +26,38 @@ def contact(request):
             contact = Contact(name=name,email=email,roll=roll,content=content)
             contact.save()
             messages.success(request,'Success! Your response has been recorded')
-    return render(request,'home/contact.html')
+    return render(request,'contact.html')
+
+
+def handleSignup(request):
+    if request.method == 'POST':
+        username= request.POST['username']
+        fname= request.POST['fname']
+        lname= request.POST['lname']
+        email= request.POST['email']
+        pass1= request.POST['pass1']
+        pass2= request.POST['pass2']
+        
+        
+        if len(username)>15:
+            messages.error(request, 'Your Username must be at least 15 characters')
+            return redirect('home')
+        if not username.isalnum():
+            messages.error(request, 'username should only contain alphabetic and alphanumeric characters')
+            return redirect('home')
+        
+        if pass1 != pass2:
+            messages.success(request, 'Confirm password does not match')
+            return redirect('home')
+        
+        #create the user
+        myuser=User.objects.create_user(username,email,pass1)
+        myuser.first_name=fname
+        myuser.last_name=lname
+        myuser.save()
+        messages.success(request, 'Your account has been successfully created.')
+        return redirect('home')
+    
+    else:
+        return HttpResponse('404 Not Found')
+    
