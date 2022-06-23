fe9a18fa0b19f18690f1fe509de23f20ef755ce6
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-23 16:15:09 +0300

    cretated blogpost model

--- blogpost/models.py
+++ blogpost/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+
+class Post(models.Model):
+    title=  models.CharField(max_length=255, unique=True),
+    slug = models.SlugField(max_length=200, unique=True),
+    author=  models.CharField(max_length=255, unique=True),
+    content=  models.TextField()
+    updated_on = models.DateTimeField(auto_now= True)
+    images= models.ImageField(upload_to='images',default='default.png')
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    class Meta:
+            ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+    
+    
+    
+class BlogComment(models.Model):
+    comment=  models.TextField(max_length=255, blank=True),
+    user= models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
+    post=  models.ForeignKey(Post, blank=True,on_delete = models.CASCADE)
